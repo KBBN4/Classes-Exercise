db8dae9f476cce53d8da13448a2d23cb65bb8c8b
katherine Buendia <kbbuendia24@gmail.com>
2023-04-20 10:49:12 -0400

    My Classes Excercise

---
+++ Classes/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Classes
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+       
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+        
+
+    }
+}
+
+
+	
+
+
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+
+            myCar.Make = " Honda";
+            myCar.Model = "Accord";
+            myCar.Year = "2050";
+
+            Console.WriteLine(myCar.Make);
+            Console.WriteLine(myCar.Model);
+            Console.WriteLine(myCar.Year);
+
+
+            Car yourCar = new Car() { Make = " Bugatti", Model = "Unknown", Year = "2030" };
+
+            Console.WriteLine();
+            Console.WriteLine($"My {yourCar.Make} is better or better looking than your {myCar.Make}");
+            
+            
+
+               
+                
+                
         }
     }
 }
